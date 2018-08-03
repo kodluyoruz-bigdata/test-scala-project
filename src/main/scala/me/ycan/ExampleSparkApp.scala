692cbab437ba956f36736c0b3cd319b0b86aa3e0
Yaşarcan Yılmaz <yasarcanyilmaz@yahoo.com.tr>
2018-08-03 20:47:11 +0300

    Add 3rd Aug examples

--- src/main/scala/me/ycan/ExampleSparkApp.scala
+++ src/main/scala/me/ycan/ExampleSparkApp.scala
@@ -5,7 +5,7 @@ import org.apache.spark.rdd.RDD
 
 import scala.util.Try
 
-case class RawData(a: String, b: String, c: String)
+case class RawData(a: String, b: String, c: Any)
 case class GroupedMyMotherName(mother: String, count: Long){
   override def toString(): String= s"Mother name: $mother, Count: $count"
 }
@@ -32,18 +32,68 @@ object ExampleSparkApp extends App{
     .filter(maybeRawData => maybeRawData.isSuccess)
     .map(data => data.get)
 
-//  filteredRawData.map(r => (r.a, r)).reduceByKey{ t =>
-//
+  val result = filteredRawData
+    .map(r => (r.a, r.c))
+    .reduceByKey { case (value1: String, value2: String) => s"$value1 $value2"
+    }
+
+
+//  val result: RDD[GroupedMyMotherName] = filteredRawData
+//    .groupBy(_.a).map{ case (key, values) =>
+//    GroupedMyMotherName(key, values.size)
 //  }
 
-
-  val result: RDD[GroupedMyMotherName] = filteredRawData.groupBy(_.a).map{ case (key, values) =>
-    GroupedMyMotherName(key, values.size)
-  }
-
-  val collectedResult: Array[GroupedMyMotherName] = result.collect()
+  val collectedResult: Array[(String, String)] = result.collect()
 
   collectedResult.foreach(println)
+
+
+  case class A(name: String, surname:String, childName: String)
+
+
+
+  val example2Data = sparkContext.textFile(path = "/Users/ycan/Desktop/test.txt")
+
+  val mappedData = example2Data.map{ line =>
+
+    val fields = line.split(";")
+
+    val childNames = fields(2).split(",")
+
+    childNames.map{ child =>
+      A(fields(0), fields(1), child)
+    }
+  }
+
+  val testData = mappedData.flatMap(r => r)
+
+  testData
+
+
+  case class B(name: String, surname:String, childName: Array[String])
+
+
+  val testData2 = example2Data.map{ line =>
+    val fields = line.split(";")
+
+    val childNames = fields(2).split(",")
+
+    B(fields(0), fields(1), childNames)
+
+  }
+
+  val testData3 = testData2.map{ b =>
+    b.childName.map{ child =>
+      A(b.name, b.surname, child)
+    }
+  }
+
+  val testArray = List("a", "b", "b", "c").toArray
+
+  val testRDD = sparkContext.parallelize(testArray)
+
+  testRDD.countByValue()
+
 
 
 
